df4fde7ca65c2e97215ea66a5641a84c5d841461
Joel Stennett <joelstennett17@gmail.com>
2024-01-23 22:32:33 -0600

    fixed UI with style sheet

--- app/settings.py
+++ app/settings.py
@@ -110,7 +110,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_REDIRECT_URL = '/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
